60cdc3707eab8f353f7eb04c1a9edeb961b040c6
Michael Sorensen <voltx180@gmail.com>
2018-07-30 00:35:15 -0700

    forgot about CORS HOW CAN YOU FORGET ABOUT CORS REEEEEEEEeeeeeeeeeeeeee

--- server.js
+++ server.js
@@ -12,15 +12,15 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(compression());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Credentials", true);
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Credentials", true);
+//   next();
+// });
 
 /* for passport */
 
